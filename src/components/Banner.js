5ae73a6bef9ee35f34fa3876703356a35cd53135
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2023-04-08 13:17:18 +0100

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -114,12 +114,7 @@ export default Banner;
 
 
 
-
-const Banner = () => {
-  return (
-    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center bg-center" id="home">
-      <div className="container mx-auto">
-        <div className="flex flex-col gap-y-8 lg:flex-row
+flex-col gap-y-8 lg:flex-row
          lg:items-center lg:gap-x-12">
           {/*{text}*/}
           <div className="flex-1 text-center font-secondary lg:text-left ">
